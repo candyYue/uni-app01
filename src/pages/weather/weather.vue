13618460d063a61566d71eafc0133f958e83e8bb
dongyue <1006294768@qq.com>
2021-12-02 00:05:48 +0800

    add icons

--- src/pages/weather/weather.vue
+++ src/pages/weather/weather.vue
@@ -6,11 +6,12 @@
 	export default{
 		methods:{
 			getWeather(){
+				console.log('get')
 				uni.request({
 					url:'/juheweather/simpleWeather/query',
 					data:{
 						key:'7f2e66a7ae64c23103847f0415a09571',
-						city:encodeURI('南京')
+						city:'南京'
 					},
 					header: {
 					    'content-type': 'application/x-www-form-urlencoded'
---
+++ src/utils/config/type.js
@@ -0,0 +1,5 @@
+const  weather = {
+	'晴': 'sun',
+	'阴': 'cloudy',
+	'雨': 'rainy'
+}
